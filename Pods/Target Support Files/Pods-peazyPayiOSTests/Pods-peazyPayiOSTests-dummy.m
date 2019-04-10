276a5e5cdb1bdc5d170b4b8dd65ad8829bf3d2fb
Elden Li <lielden@yahoo.com>
2019-04-09 17:46:31 -0700

    connected to Firebase

---
+++ Pods/Target Support Files/Pods-peazyPayiOSTests/Pods-peazyPayiOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_peazyPayiOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_peazyPayiOSTests
+@end
---
+++ Pods/Target Support Files/Pods-peazyPayiOSUITests/Pods-peazyPayiOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_peazyPayiOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_peazyPayiOSUITests
+@end
